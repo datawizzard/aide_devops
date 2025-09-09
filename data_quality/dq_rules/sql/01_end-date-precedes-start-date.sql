bfad3292e90a72a2d01f16f55e164a69888b69ff
Divya Anand <104557427+datawizzard@users.noreply.github.com>
2025-09-10 01:47:36 +0530

    Delete data_quality/dq_rules/sql directory

--- data_quality/dq_rules/sql/01_end-date-precedes-start-date.sql
+++
@@ -1,6 +0,0 @@
-SELECT `calendar_id`,
-       `start_date`,
-       `end_date`,
-       'end_date precedes start_date' AS violation_reason
-FROM `aigdqr-ragstoriches`.`calendar_ddp`.`v_calendar`
-WHERE `end_date` < `start_date`
--- data_quality/dq_rules/sql/02_end-date-significantly-far-in-the-future.sql
+++
@@ -1,5 +0,0 @@
-SELECT `calendar_id`,
-       `end_date`,
-       'end_date is significantly far in the future' AS violation_reason
-FROM `aigdqr-ragstoriches`.`calendar_ddp`.`v_calendar`
-WHERE `end_date` > date_add(current_date(), 36500)
